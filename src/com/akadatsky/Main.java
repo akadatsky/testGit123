b931f59678da21a4b6bcbb68be45e30e517a8d04
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2019-07-20 12:30:44 +0300

    REmovee some code

--- src/com/akadatsky/Main.java
+++ src/com/akadatsky/Main.java
@@ -1,13 +1,8 @@
 package com.akadatsky;
 
-import static java.lang.Math.sqrt;
-
 public class Main {
 
     public static void main(String... args) {
-
-        double d = sqrt(10);
-
         int result = sum(10, 20, 30, 40, 50, 60);
         result = sum(10, 20);
 
